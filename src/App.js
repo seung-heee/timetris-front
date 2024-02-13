8942838150ac65b62120b7b69e7e7d12495035a1
Jieun Lim <jieun776121@gmail.com>
2024-02-13 13:16:32 +0900

    feat: login, mypage 경로 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Main from "./pages/Main";
+import Login from "./pages/Login/Login"
+import MyPage from "./pages/MyPage/MyPage"
 import './css/styles.css';
 
 function App() {
@@ -7,6 +9,8 @@ function App() {
     <div className="App container">
       <Routes>
         <Route path='/' element={<Main />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/mypage' element={<MyPage />} />
       </Routes>
     </div>
   );
