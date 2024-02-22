a0fddb47f9ec14a5783f306995a307c836788fb5
Jieun Lim <jieun776121@gmail.com>
2024-02-23 03:10:34 +0900

    feat: api 데이터 Table 위에 표현

--- src/pages/Pds/PdsTable.js
+++ src/pages/Pds/PdsTable.js
@@ -4,6 +4,8 @@ import styled from 'styled-components'
 import Plan from './Plan';
 import Do from './Do';
 import See from './See';
+// import { PDSTableContext } from '../../context/PDSTableContext';
+// import data from './api연습'
 
 const TableContainer = styled.div`
     display : flex;
@@ -16,7 +18,6 @@ const ToolTipContainer = styled.div`
     cursor : pointer;
     justify-content : space-between;
 `
-
 const ToolTip = styled.div`
     width: 400px;
     height: 49px;
@@ -40,7 +41,6 @@ const BtnContainer = styled.div`
     display : flex;
     justify-content : flex-end;
 `
-
 const PDSContainer = styled.div`
     margin-top : 16px;
     display : flex;
@@ -58,6 +58,13 @@ const BottomContainer = styled.div`
 `
 
 const PdsTable = () => {
+    // // 1. main data GET!
+    // // data 박아놓기
+    // console.log(data.result.doViewDTOList[0].title)
+    // const planData = data.result.planViewDTOList[0]
+    // const doData = data.result.doViewDTOList[0]
+    // const seeData = data.result.seeViewDTO[0]
+
     const [showAlert, setShowAlert] = useState(false);
 
     // 버튼을 누를 때 알림 보이기
--- src/pages/Pds/Plan.js
+++ src/pages/Pds/Plan.js
@@ -1,4 +1,7 @@
 import styled from "styled-components"
+import * as API from '../../api/API'
+import { PDSTableContext } from '../../context/PDSTableContext';
+import { useContext, useState } from "react";
 
 const PlanContainer = styled.div`
 
@@ -43,6 +46,8 @@ const PlanTableCell = styled.td`
 `
 
 const Plan = () => {
+    const { planData } = useContext(PDSTableContext)
+
     const Time = ["4AM", "5AM", "6AM", "7AM", "8AM", "9AM", "10AM", "11AM", "12PM",
         "1PM", "2PM", "3PM", "4PM", "5PM", "6PM", "7PM", "8PM", "9PM", "10PM"
         , "11PM", "12AM", "1AM", "2AM", "3AM"]
@@ -59,14 +64,54 @@ const Plan = () => {
         }
         tableData.push(rowData);
     }
-    console.log(tableData)
 
-    // 각 행을 나타내는 JSX 배열을 생성합니다.
+    const checkColoredCell = () => {
+        let normalizedStartHour = planData.startTime.hour;
+        let normalizedEndHour = planData.endTime.hour;
+
+        if (planData.startTime.hour >= 4 && planData.startTime.hour <= 24) {
+            normalizedStartHour = planData.startTime.hour - 4;
+        } else if (planData.startTime.hour >= 1 && planData.startTime.hour <= 3) {
+            normalizedStartHour = planData.startTime.hour + 20;
+        }
+
+        if (planData.endTime.hour >= 4 && planData.endTime.hour <= 24) {
+            normalizedEndHour = planData.endTime.hour - 4;
+        } else if (planData.endTime.hour >= 1 && planData.endTime.hour <= 3) {
+            normalizedEndHour = planData.endTime.hour + 20;
+        }
+        const planedCellIndex = []
+        if (normalizedStartHour > normalizedEndHour) {
+            for (let i = normalizedStartHour; i < 24; i++) {
+                planedCellIndex.push(i)
+            }
+            for (let j = 0; j <= normalizedEndHour; j++) {
+                planedCellIndex.push(j)
+            }
+        } else {
+            for (let i = normalizedStartHour; i < normalizedEndHour + 1; i++) {
+                planedCellIndex.push(i)
+            }
+        }
+        return [normalizedStartHour, planedCellIndex]
+    }
+    // 각 행을 나타내는 JSX 배열을 생성
     const tableRows = tableData.map((rowData, rowIndex) => (
         <PlanTableRow key={rowIndex}>
-            {rowData.map((colData, colIndex) => (
-                <PlanTableCell key={colIndex}>{colData}</PlanTableCell>
-            ))}
+            {rowData.map((colData, colIndex) => {
+                // 특정 조건에 따라 배경색을 설정
+                let [normalizedStartHour, planedCellIndex] = checkColoredCell()
+                const isColoredCell = colIndex == 0 && planedCellIndex.includes(rowIndex)
+                const isStartTimeCell = colIndex == 0 && rowIndex == normalizedStartHour;
+                return (
+                    <PlanTableCell key={colIndex} style={{ backgroundColor: isColoredCell ? planData.category.colorCode : '#F6F6F6' }}>
+                        {isStartTimeCell
+                            ? planData.title : ""
+                        }
+                        {colData}
+                    </PlanTableCell>
+                );
+            })}
         </PlanTableRow>
     ));
 
@@ -83,6 +128,19 @@ const Plan = () => {
                     {tableRows}
                 </tbody>
             </PlanTable>
+            {/* <CategoryContext.Provider
+                value={{
+                    state, setState, ModalHandler, selectColorCode,
+                    addCategory, setAddCategory, HandleAddCategory,
+                    selectedDay, handleRepeatCheck,
+                    categoryId, setCategoryId,
+                    myCategory, setMyCategory, ShowCategoryList,
+                    fixCategory, setFixCategory,
+                    addPlan, setAddPlan,
+                    doPlan, setDoPlan
+                }}>
+                <PlanModal />
+            </CategoryContext.Provider> */}
         </PlanContainer>
     )
 }
